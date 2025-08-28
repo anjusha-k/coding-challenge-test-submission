5dd2bc9a2e564a0deee2bcc92949edef9a397503
anjusha-k <anjusha.khandavalli@gmail.com>
2025-08-28 22:27:39 +1000

    Add Roboto font from Google Fonts with --font-primary CSS variable

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@/styles/global.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
